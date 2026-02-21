18936d6127b4b12d1c6e7738590d78af92c9c2f4
ahadunnobi <hacker.ven10@gmail.com>
2026-02-22 05:28:06 +0600

    toggle added to app

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
+let interviewList = [];
+let rejectedList = [];
+
 let totalJob = document.getElementById('totalJob');
 let allJob = document.getElementById('AllJob');
 let Interview =document.getElementById('Interview');
 let Rejected=document.getElementById('Rejected');
-let alljob 
+
 
 let getTotalJob=document.getElementById('jobs');
+let main = document.querySelectorAll('main');
+
+let btnAllJob =document.getElementById('btn-alljob');
+let btnInterview =document.getElementById('btn-Interview');
+let btnRejected =document.getElementById('btn-Rejected');
+
 function calculateJob(){
     totalJob.innerText = getTotalJob.children.length;
     allJob.innerText = getTotalJob.children.length;
+    Interview.innerText = interviewList.length;
+    Rejected.innerText = rejectedList.length;
 }
 calculateJob()
+function toggle(id){
+    btnAllJob.classList.remove('bg-blue-800', 'text-white');
+    btnInterview.classList.remove('bg-blue-800', 'text-white');
+    btnRejected .classList.remove('bg-blue-800', 'text-white');
+
+    btnAllJob.classList.add('bg-gray-300', 'text-black');
+    btnInterview.classList.add('bg-gray-300', 'text-black');
+    btnRejected .classList.add('bg-gray-300', 'text-black');
+    const selected = document.getElementById(id);
+    selected.classList.remove('bg-gray-300', 'text-black');
+    selected.classList.add('bg-blue-800', 'text-white');
+}
